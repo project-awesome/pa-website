105cfa7d4e78216826820bc026933a914e8aa3f6
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-08-20 12:56:51 -0700

    QuizDescriptor model now includes a hidden property with default value false.

--- models/QuizDescriptor.js
+++ models/QuizDescriptor.js
@@ -1,7 +1,8 @@
 module.exports = function(sequelize, DataTypes) {
 	var QuizDescriptor =  sequelize.define('QuizDescriptor', {
 		id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-		descriptor: DataTypes.JSONB
+		descriptor: DataTypes.JSONB,
+		hidden: { type: DataTypes.BOOLEAN, defaultValue: false }
 	}, {
 		classMethods: {
 			associate: function(models) {
