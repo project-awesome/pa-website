8915ba766d285cf2758ad5c6e2f777b0f904f800
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-08-27 12:11:29 -0700

    Changed form defintion to ['*']

--- public/javascript/angular/services.js
+++ public/javascript/angular/services.js
@@ -76,7 +76,7 @@ awesomeApp.factory('PAQuestions', [function() {
                     }
                 }
             },
-            form: [],
+            form: ["*"],
             template: {
                 repeat: 1,
                 question: 'binHexOctDec',
@@ -94,7 +94,7 @@ awesomeApp.factory('PAQuestions', [function() {
                     repeat: { type: "integer", title: "Repeat" }
                 }
             },
-            form: [],
+            form: ["*"],
             template: {
                 repeat: 1,
                 question: 'changeOfBase'
